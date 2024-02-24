1fb0615de9d108ab29c957b99ffdc5170019dc44
Rajeev Yadav <ry.rajeevyadav12@gmail.com>
2024-02-24 18:18:42 +0530

    feat: added calculation function

---
+++ shape.js
@@ -0,0 +1,14 @@
+// add function
+function add(a, b) {
+    return a + b;
+}
+
+// subtract function
+function subtract(a, b) {
+    return a - b;
+}
+
+// multiply function
+function multiply(a, b) {
+    return a * b;
+}
